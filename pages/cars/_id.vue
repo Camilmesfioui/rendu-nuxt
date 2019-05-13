0a8814545b27fcf2ae7a79232ad328a1dd9ddb39
Arthur Leroux <arthur_leroux@hotmail.fr>
2019-05-13 16:48:18 +0200

    Affichage voitures

---
+++ pages/cars/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1>{{ currentCar.brand }}</h1>
+    <p>
+      {{ currentCar.model }}
+    </p>
+    <nuxt-link :to="{name: 'cars'}">
+      <button>Retour</button>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+  import axios from "~/plugins/axios";
+
+  export default {
+    async asyncData({ params }) {
+      const { data } = await axios.get(`/cars/${params.id}`);
+
+      return {
+        currentCar: data
+      }
+    },
+
+    data() {
+      return {
+        currentCar: null
+      }
+    },
+
+    head() {
+      return {
+        title: `${this.currentCar.brand} | Mon site internet`,
+        meta: [
+          {}
+        ]
+      }
+    }
+  }
+</script>
---
+++ pages/cars/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="car in cars" :key="car.id">
+        <nuxt-link :to="{name: 'cars-id', params: {id: car.id}}">
+          {{ car.brand }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from "~/plugins/axios";
+
+  export default {
+    async asyncData() {
+      const { data } = await axios.get('/cars');
+
+      return {
+        cars: data
+      }
+    },
+    data() {
+      return {
+        title: "Cars",
+        cars: []
+      }
+    }
+  }
+</script>
