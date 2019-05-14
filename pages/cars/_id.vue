0fda13933465cb19485e48cafb057771441be02a
Camil Mesfioui <camil.mesfioui@gmail.com>
2019-05-14 11:54:45 +0200

    Edit cars

--- pages/cars/_id.vue
+++ pages/cars/_id.vue
@@ -4,6 +4,9 @@
     <p>
       {{ currentCar.model }}
     </p>
+    <input v-model="brand" type="text" placeholder="brand">
+    <input v-model="model" type="text" placeholder="model">
+    <button v-on:click="edit">Modify</button>
     <nuxt-link :to="{name: 'cars'}">
       <button>Retour</button>
     </nuxt-link>
@@ -14,6 +17,18 @@
   import axios from "~/plugins/axios";
 
   export default {
+    methods: {
+      edit: function () {
+        axios.put(`/cars/${this.currentCar.id}`, {
+          brand: this.brand,
+          model: this.model
+        })
+        .then(function(response){
+          location.reload();
+        });
+      }
+    },
+
     async asyncData({ params }) {
       const { data } = await axios.get(`/cars/${params.id}`);
 
