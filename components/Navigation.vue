a1f634230ef8e8fb50d62d9fe1bb1bd9c8c6c55b
Arthur Leroux <arthur_leroux@hotmail.fr>
2019-05-15 12:16:20 +0200

    Recherche

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -15,6 +16,28 @@
           </nuxt-link>
         </li>
       </ul>
+      <div class="form-inline my-2 my-lg-0">
+        <input v-model="q" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button @click="search" class="btn btn-outline-success my-2 my-sm-0">Search</button>
+      </div>
     </div>
   </nav>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        q: null,
+      }
+    },
+    methods: {
+      search() {
+        if (this.q) {
+          this.$router.push({name: 'search-q', params: {q: this.q}})
+        }
+      }
+    }
+  }
+</script>
+
