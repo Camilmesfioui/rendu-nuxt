eb84fbacdbe20760dbf518698594a8aca04a19e5
Arthur Leroux <arthur_leroux@hotmail.fr>
2019-05-15 20:26:23 +0200

    Dynamisation img et details

--- components/Car.vue
+++ components/Car.vue
@@ -2,7 +2,7 @@
     <div class="card col-4 col-md-4 col-sm-4 col-xs-4">
         <img class="card-img-top" :src="car.imgLink" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">{{ car.brand }}</h5>
+            <h5 class="card-title">{{ car.brand }}, {{ car.model }}</h5>
             <p class="card-text">{{ car.detail }}</p>
         </div>
         <nuxt-link :to="{name: 'cars-id', params: {id: car.id}}">
